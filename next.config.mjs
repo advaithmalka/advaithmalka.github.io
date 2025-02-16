c48e2f7b1f3b7a18f3cac9ae123f804f519ee4d2
Advaith Malka <advaithm06@gmail.com>
2025-02-15 20:41:42 -0500

    Deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "",
+    basePath: "/",
     images: {
         unoptimized: true,
     },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
