ad20cc0f4b156c3cbbf9aa8a28123ded18a3839e
Advaith Malka <advaithm06@gmail.com>
2025-04-01 23:28:11 -0400

    add nc-ratio project

--- src/app/page.js
+++ src/app/page.js
@@ -9,21 +9,36 @@ import Card from "@/components/Card";
 export default function Home() {
     const [projectCards, setProjectCards] = useState([]);
 
+    const urlsToPing = [
+        "https://advaithmalka-nc-calculator-api.hf.space/",
+        "https://advaithmalka-cristae-detect-api.hf.space/",
+        "https://advaithmalka-mito-detect-api.hf.space/"
+    ]
+
     useEffect(() => {
         // Get up to 3 featured projects
         setProjectCards(
-          data.slice(0, 3).map((project, idx) => (
-              <Card
-                  key={idx}
-                  title={project.name}
-                  desc={project.description}
-                  link={project.link}
-                  img={project.img}
-                  date={project.date}
-                  badges={project.badges.slice().reverse()}
-              />
-          ))
+            data
+            .filter((_, idx) => idx === 0 || idx === 1 || idx === 3)
+            .map((project, idx) => (
+                <Card
+                    key={idx}
+                    title={project.name}
+                    desc={project.description}
+                    link={project.link}
+                    img={project.img}
+                    date={project.date}
+                    badges={project.badges.slice().reverse()}
+                />
+            ))
       );
+
+        urlsToPing.forEach(async (url) => {
+            const response = await fetch(url, { method: "GET" });
+            console.log(`Pinged ${url} - Status: ${response.status}`);
+        });
+   
+      
     }, []);
 
  
